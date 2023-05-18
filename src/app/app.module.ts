49e9de37efdfe29259f3108fe38c73e80f89234e
Pai <pai.nuneza@gmail.com>
2023-05-18 11:53:34 +0800

    ghpages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactMeComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule
