cd3301af63e5798991a4153430003597910d9823
Pai <pai.nuneza@gmail.com>
2023-05-18 13:52:42 +0800

    Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactMeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
