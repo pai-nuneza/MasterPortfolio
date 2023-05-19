860e8744daa8a4a9b8d6728f216df8d4279ed536
Pai <pai.nuneza@gmail.com>
2023-05-19 12:40:09 +0800

    Updates

--- src/app/resume/about-me/about-me.component.ts
+++ src/app/resume/about-me/about-me.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.css']
+  styleUrls: ['./about-me.component.css'],
 })
 export class AboutMeComponent {
+  aboutMe =
+    "Welcome to my About Me page! I'm thrilled to share my journey and experiences with you. My name is Pai, and I have been working in the software industry since 2015. With a total of 8 years of experience, I have had the opportunity to work on diverse projects and expand my knowledge in various technologies.";
+  coreExpertise =
+    'One of my core areas of expertise is in C# and Angular technologies. I have a strong command over these languages and enjoy creating robust and efficient software solutions using them. Additionally, I have hands-on experience with AWS and Azure cloud platforms, Git for version control, and MSSQL for database management. This diverse skill set allows me to tackle complex challenges and deliver high-quality results.';
+  outsideProfessionalWorld =
+    "Outside of the professional world, I have a few passions that keep me energized and well-rounded. I am an avid video gamer and love exploring virtual worlds and engaging in immersive gameplay experiences. It's a fantastic way to unwind and challenge my problem-solving skills.";
+  digitalRealm =
+    "While I'm engrossed in the digital realm, I also value physical fitness. You'll often find me hitting the gym, pushing my limits, and striving for a healthier lifestyle. I believe that maintaining a balance between mental and physical well-being is essential for overall success and happiness.";
+  music =
+    "Music is another significant part of my life. I have been playing the guitar for several years now. There's something magical about strumming the strings and creating melodies that evoke emotions. It's a creative outlet that allows me to express myself in a unique way.";
+  parenting =
+    'Lastly, I am a proud parent to an amazing child. Being a parent has been an incredible journey filled with joy, love, and constant learning. It has taught me patience, responsibility, and the importance of nurturing young minds.';
+  end =
+    "As you browse through my website, you'll discover more about my professional achievements, projects I have worked on, and my passion for technology. Don't hesitate to reach out if you have any questions or if you would like to collaborate on exciting opportunities. Thank you for visiting my About Me page, and I hope you enjoy exploring my world!";
+
 
 }
