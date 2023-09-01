a6c7ba0761f77c9158b98a0e3a28a185035ae04a
Pai <pai.nuneza@gmail.com>
2023-09-02 00:27:09 +0800

    Major Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,6 @@ export class AppComponent {
   constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    // particlesJS.load('particles-js', '/assets/particles.json', null);
+    particlesJS.load('particles-js', '/assets/particles.json', null);
   }
-
-  // This code will put glassy when scrolling down
-  // @HostListener('window:scroll', [])
-  // onWindowScroll() {
-  //   const navbar = document.querySelector('.navbar');
-  //   if (window.pageYOffset > 0) {
-  //     this.renderer.addClass(navbar, 'glassy');
-  //   } else {
-  //     this.renderer.removeClass(navbar, 'glassy');
-  //   }
-  // }
 }
