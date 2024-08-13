5f27ed5bbd4e6a0c63352c33cd447b71a5f1ad04
Pai <pai.nuneza@gmail.com>
2024-08-14 02:12:39 +0800

    test

--- src/app/resume/projects/projects.component.ts
+++ src/app/resume/projects/projects.component.ts
@@ -12,17 +12,17 @@ export class ProjectsComponent {
       text: 'Booking/Boat Hiring',
       buttonText: 'Go to site',
       link: 'https://sydney-boats.netlify.app',
-      tech: 'Angular, Bootstrap',
-      imgLink: 'https://images.unsplash.com/photo-1595013694368-aab1b4f70dcd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2001&q=80', 
+      tech: 'Angular 17, Bootstrap 5',
+      imgLink: 'https://images.unsplash.com/photo-1562281302-809108fd533c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
     },
-    // {
-    //   title: 'Inventory System',
-    //   text: 'For small business',
-    //   buttonText: 'Go to site',
-    //   link: '#',
-    //   tech: 'ASP.NET',
-    //   imgLink: 'https://images.unsplash.com/photo-1616401784845-180882ba9ba8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
-    // },
+    {
+      title: 'Gamevault',
+      text: 'Video Games, Online, Multiplayer',
+      buttonText: 'Site - In Progress',
+      link: '#',
+      tech: 'Vue 3, Steam API',
+      imgLink: 'https://www.91-cdn.com/hub/wp-content/uploads/2021/12/ar88u.jpg'
+    },
     // {
     //   title: 'Online Quiz Game',
     //   text: 'Have fun',
