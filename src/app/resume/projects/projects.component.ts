1f02126f54b8b06ec813eff8c29c11701fba0286
Pai <pai.nuneza@gmail.com>
2025-12-02 22:39:24 +0800

    small updates

--- src/app/resume/projects/projects.component.ts
+++ src/app/resume/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Project } from '../../models/project.model';
 import projectsData from '../../../assets/data/projects-enhanced.json';
@@ -7,10 +7,23 @@ import projectsData from '../../../assets/data/projects-enhanced.json';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
 })
-export class ProjectsComponent {
-  items: Project[] = projectsData as Project[];
+export class ProjectsComponent implements OnInit {
+  items: Project[] = [];
 
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.items = this.shuffleArray([...projectsData as Project[]]);
+  }
+
+  shuffleArray(array: Project[]): Project[] {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  }
 
   viewProject(projectId: string, event: Event): void {
     event.preventDefault();
