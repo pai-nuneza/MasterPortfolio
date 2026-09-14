3a238ad0f4afbf55ab56939e41bc882e6b0a1495
Pai <pai.nuneza@gmail.com>
2026-09-14 10:00:34 +0800

    sd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,6 @@ import { Component, HostBinding, HostListener } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  @HostBinding('style.--scroll-depth')
-  scrollDepth = '0px';
-
-  @HostBinding('style.--scroll-tilt')
-  scrollTilt = '0deg';
-
   @HostBinding('style.--meteor-opacity')
   meteorOpacity = '1';
 
@@ -27,8 +21,6 @@ export class AppComponent {
     window.requestAnimationFrame(() => {
       const scrollY = window.scrollY || 0;
 
-      this.scrollDepth = `${Math.min(scrollY * 0.12, 140)}px`;
-      this.scrollTilt = `${Math.max(Math.min(scrollY * 0.002, 1.8), -1.8)}deg`;
       this.meteorOpacity = `${Math.max(1 - scrollY / 420, 0)}`;
       this.ticking = false;
     });
